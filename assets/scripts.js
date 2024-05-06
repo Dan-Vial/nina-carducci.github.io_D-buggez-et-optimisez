2d4726b5488759f6feed26ba56a65f8276d5a6f6
Dan VIAL <dan.vial@dbmail.com>
2024-05-06 16:26:21 +0200

    debugging optimized

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,61 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+window.addEventListener('load', async function () {
+    await execAllPreload();
+
+    $(document).ready(function () {
+        $('.gallery').mauGallery({
+            columns: {
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 3,
+                xl: 3
+            },
+            lightBox: true,
+            lightboxId: 'myAwesomeLightbox',
+            showTags: true,
+            tagsPosition: 'top'
+        });
     });
 });
+
+async function execAllPreload() {
+    let range = document.createRange();
+    let allFontPreload = document.querySelectorAll('link[rel="preload"][as="font"]');
+    let allCssPreload = document.querySelectorAll('link[rel="preload"][as="style"]');
+    let allScriptPreload = document.querySelectorAll('link[rel="preload"][as="script"]');
+
+    while (allFontPreload.length > 0) {
+        allFontPreload[0].rel = 'stylesheet';
+        delete allFontPreload[0].as;
+        allFontPreload = document.querySelectorAll('link[rel="preload"][as="font"]');
+    }
+
+    while (allCssPreload.length > 0) {
+        allCssPreload[0].rel = 'stylesheet';
+        delete allCssPreload[0].as;
+        allCssPreload = document.querySelectorAll('link[rel="preload"][as="style"]');
+    }
+
+    while (allScriptPreload.length > 0) {
+        let script = range.createContextualFragment(`
+            <script src="${allScriptPreload[0].href}" defer></script>
+        `);
+        document.head.appendChild(script);
+        allScriptPreload[0].remove();
+
+        allScriptPreload = document.querySelectorAll('link[rel="preload"][as="script"]');
+
+        let started = true;
+        var ended = true;
+        while (ended) {
+            if (started) {
+                started = false;
+                ended = await new Promise((resolve) => {
+                    document.head.lastElementChild.addEventListener('load', event => {
+                        resolve(false);
+                    });
+                });
+            }
+        }
+    }
+}
